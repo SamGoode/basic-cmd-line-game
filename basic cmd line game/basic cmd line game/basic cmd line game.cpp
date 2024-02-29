c9f45577824150536b047775ab4acedec95f2a9b
Jevan Soh <s240150@students.aie.edu.au>
2024-02-29 17:07:25 +1100

    bunch of issues

--- basic cmd line game/basic cmd line game/basic cmd line game.cpp
+++ basic cmd line game/basic cmd line game/basic cmd line game.cpp
@@ -3,22 +3,30 @@
 
 class Item {
 private:
+    String name;
     String description;
 
 public:
     Item() {
+        name = "???";
         description = "???";
     }
 
     Item(const Item& item) {
-        this->description = item.getDescription();
+        this->name = item.name;
+        this->description = item.description;
     }
 
-    Item(String description) {
+    Item(String name, String description) {
+        this->name = name;
         this->description = description;
     }
 
     //virtual ~Item() = default;
+
+    String& getName() {
+        return name;
+    }
 
     String& getDescription() {
         return description;
@@ -28,64 +36,39 @@ public:
         return description;
     }
 
+    Item& operator=(const Item& item) {
+        this->name = item.name;
+        this->description = item.description;
+
+        return *this;
+    }
+
     void Use() {
 
     }
 };
 
-//class ItemList {
-//private:
-//    size_t itemCount;
-//    Item* items;
-//
-//public:
-//    ItemList() {
-//        itemCount = 0;
-//        items = nullptr;
-//    }
-//
-//    ItemList(const ItemList& itemList) {
-//        this->itemCount = itemList.length();
-//        for (int i = 0; i < itemList.length(); i++) {
-//            this->items[i] = itemList.getItem(i);
-//            //itemList.getItem(i) = Item();
-//        }
-//    }
-//
-//    ItemList(size_t itemCount, Item* items) {
-//        this->itemCount = itemCount;
-//        std::cout << itemCount;
-//        for (int i = 0; i < itemCount; i++) {
-//            //this->items[i] = Item(items[i]);
-//        }
-//        std::cout << "test";
-//    }
-//
-//    ~ItemList() {
-//        delete[] items;
-//    }
-//
-//    Item& getItem(size_t index) const {
-//        return items[index];
-//    }
-//
-//    size_t length() const {
-//        return itemCount;
-//    }
-//};
-
 class Room {
-private:
+public:
     String description;
     size_t itemCount;
     Item* items;
-    //ItemList items;
     
 public:
     Room() {
         description = "empty room";
         itemCount = 0;
         items = nullptr;
+    }
+
+    Room(const Room& room) {
+        this->description = room.description;
+        
+        this->itemCount = room.itemCount;
+        this->items = new Item[room.itemCount];
+        for (int i = 0; i < room.itemCount; i++) {
+            this->items[i] = room.items[i];
+        }
     }
 
     Room(String description) {
@@ -96,20 +79,41 @@ public:
 
     Room(String description, size_t itemCount, Item* items) {
         this->description = description;
+
         this->itemCount = itemCount;
+        this->items = new Item[itemCount];
         for (int i = 0; i < itemCount; i++) {
-            std::cout << "assigning item " << i << std::endl;
-            *(this->items + i) = *(items + i);
+            this->items[i] = items[i];
         }
+
+        //delete[] items;
+    }
+
+    ~Room() {
+        delete[] this->items;
+    }
+
+    Room& operator=(const Room& room) {
+        this->description = room.description;
+
+        this->itemCount = room.itemCount;
+        delete[] items;
+        this->items = new Item[room.itemCount];
+        for (int i = 0; i < room.itemCount; i++) {
+            this->items[i] = room.items[i];
+        }
+
+        return *this;
     }
 
     void Description() {
         std::cout << "You entered the room\n";
         description.WriteToConsole();
-        std::cout << "Items:\n";
+        std::cout << "\nItems:\n";
         for (int i = 0; i < itemCount; i++) {
-            std::cout << "item: " << i << std::endl;
-            //items[i].getDescription().WriteToConsole();
+            items[i].getName().WriteToConsole();
+            items[i].getDescription().WriteToConsole();
+            std::cout << std::endl;
         }
     }
 };
@@ -128,16 +132,29 @@ public:
 
 int main() {
     Room rooms[3][3];
-    Item iceCube = Item("cube of ice");
     
-    Room iceRoom = Room("It's cold in here", 2, new Item[2]{ iceCube, Item("test item") });
+    //{ Room("It's cold in here", 2, new Item[2]{ iceCube, Item("test item", "I'm for a test")}) };
+    //{
+    Item iceCube = Item("ice cube", "cube of ice");
+    //    Item* items = new Item[2]{ iceCube, Item("test item", "I'm for a test") };
 
+    //    Room iceRoom = Room("It's cold in here", 2, items);//new Item[2]{ iceCube, Item("test item", "I'm for a test") });
+
+    //    delete[] items;
+
+    //    rooms[0][1] = iceRoom;
+    //}
+    rooms[0][1] = Room("It's cold in here", 2, new Item[2]{ iceCube, Item("test item", "I'm for a test") });
+    
     int currentRoom = 01;
     String userInput;
 
     while (true) {
+        //items[0].getName().WriteToConsole();
         //rooms[currentRoom / 10][currentRoom % 10].Description();
-        iceRoom.Description();
+        rooms[0][1].Description();
+        //std::cout << rooms[0][1].items[0].getDescription().CStr() << std::endl;
+        //iceRoom.Description();
 
         std::cout << "move 'north', 'east', 'south', 'west'\n";
         
